aba96a21366a2be6661b45e0d074e7b5aae9dec1
ShadrachKiplangat <55084539+sherdooh@users.noreply.github.com>
2024-03-08 16:06:50 -0500

    Async wait main program

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
 // import redisClient from './utils/redis';
 
-// (async () => {
-//     console.log(redisClient.isAlive());
-//     console.log(await redisClient.get('myKey'));
-//     await redisClient.set('myKey', 12, 5);
-//     console.log(await redisClient.get('myKey'));
-
-//     setTimeout(async () => {
-//         console.log(await redisClient.get('myKey'));
-//     }, 1000*10)
-// })();
-
 import dbClient from './utils/db';
 
 const waitConnection = () => {
